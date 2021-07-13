9a2cc177089e650d90ed0cb48544446e0b05b049
harshitvaghanicrest <87296327+harshitvaghanicrest@users.noreply.github.com>
2021-07-13 16:29:34 +0530

    Update logger.groovy

--- vars/logger.groovy
+++ vars/logger.groovy
@@ -1,17 +1,15 @@
 def info(message) {
-    script {
         echo "INFO: ${message}"
-    }
 }
 
 def debug(message) {
-    script {
+
         echo "DEBUG: ${message}"
-    }
+    
 }
 
 def error(message) {
-    script {
+
         echo "ERROR: ${message}"
-    }
+    
 }
